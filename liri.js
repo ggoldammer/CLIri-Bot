b3a05dcfe15725abedadbc2c5d9045af9922ea6d
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2018-10-10 22:11:06 -0700

    Initial setup

---
+++ liri.js
@@ -0,0 +1,29 @@
+// require("dotenv").config();
+let request = require("request");
+
+// let spotify = new Spotify(keys.spotify);
+let command = process.argv[2];
+let query = process.argv[3];
+
+switch(command){
+    case "concert-this":
+    let artist = process.argv[3];
+
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body){
+        console.log("error: ", error);
+        console.log("statusCode: ", response && response.statusCode);
+        console.log("body:" + body;
+    });
+    break;
+    case "spotify-this-song":
+
+    break;
+    case "movie-this":
+
+    break;
+    case "do-what-it-says":
+
+    break;
+    default:
+    console.log("Incorrect command. The following commands are:\n concert-this [insert song/band name]\nspotify-this-song [insert song name]\nmovie-this [insert movie name here]\ndo-what-it-says");
+}
