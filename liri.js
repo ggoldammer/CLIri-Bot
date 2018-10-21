133f48dd3faeddf068995e0b7a74a7dea5bf672e
unknown <gaboguy@gmail.com>
2018-10-21 00:50:53 -0700

    Added function for spotify and movie calls

--- liri.js
+++ liri.js
@@ -1,28 +1,28 @@
-// require("dotenv").config();
-let request = require("request");
-var moment = require('moment');
 require("dotenv").config();
-
+let request = require("request");
+let moment = require("moment");
+let fs = require("fs");
 let keys = require("./keys.js");
 let Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+require("dotenv").config();
+let omdbAPI = "trilogy";
+
+var spotifyKeys = new Spotify(keys.spotify);
 
 
-// let spotify = new Spotify(keys.spotify);
 let command = process.argv[2];
-let search = process.argv[3];
-
-if (!process.argv[3] === undefined){
-    var input = process.argv.slice(3).join(' ');
+//For loop below joins everything after index 2 into a string, allowing for multiple word inputs.
+let searchArray = [];
+for (let i = 3; i < process.argv.length; i++) {
+    searchArray.push(process.argv[i]);
 }
+var search = searchArray.join(" ");
 
 switch (command) {
+    // Search for Concert Info
     case "concert-this":
         request("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp", function (error, response, body) {
             let concertInfo = JSON.parse(body);
-            // console.log("error: ", error);
-            // console.log("statusCode: ", response && response.statusCode);
-            // console.log("body: " + concertInfo);
 
             for (let i = 0; i < 5 && i < concertInfo.length; i++) {
                 console.log('Venue: ' + concertInfo[i].venue.name + '\n' +
@@ -41,15 +41,70 @@ switch (command) {
             }
         });
         break;
+    // Search for Spotify Info
     case "spotify-this-song":
+        if (process.argv.length < 4) {
+            spotifyKeys.search({ type: 'track', query: "The Sign Ace of Base" }, function (err, data) {
+                if (err) {
+                    return console.log('Error: ' + err);
+                }
+                console.log("\nArtist: " + data.tracks.items[0].artists[0].name);
+                console.log("Song Name: " + data.tracks.items[0].name);
+                console.log("Song Preview: " + data.tracks.items[0].preview_url);
+                console.log("Album: " + data.tracks.items[0].album.name + "\n");
+            });
+            return;
+        }
+
+        spotifyKeys.search({ type: 'track', query: search }, function (err, data) {
+
+            if (err) {
+                return console.log('Error: ' + err);
+            }
+            console.log("\nArtist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[0].name);
+            console.log("Song Preview: " + data.tracks.items[0].preview_url);
+            console.log("Album: " + data.tracks.items[0].album.name + "\n");
+        });
 
         break;
+    // Search for Movie Info
     case "movie-this":
+        if (process.argv.length < 4) {
+            request("http://www.omdbapi.com/?apikey=" + omdbAPI + "&t=Mr+Nobody", function (err, response, body) {
 
+                let results = JSON.parse(body);
+
+                console.log("\nMovie Title: " + results.Title);
+                console.log("Release Year: " + results.Year);
+                console.log("IMDB Rating: " + results.Ratings[0].value);
+                console.log("Rotten Tomatoes Rating: " + results.Ratings[1].value);
+                console.log("Country Origin: " + results.Country);
+                console.log("Language: " + results.Language);
+                console.log("Plot: " + results.Plot);
+                console.log("Actors: " + results.Actors + "\n");
+            });
+            return;
+        }
+
+        request("http://www.omdbapi.com/?apikey=" + omdbAPI + "&t=" + search, function (err, response, body) {
+
+            let results = JSON.parse(body);
+
+            console.log("\nMovie Title: " + results.Title);
+            console.log("Release Year: " + results.Year);
+            console.log("IMDB Rating: " + results.Ratings[0].value);
+            console.log("Rotten Tomatoes Rating: " + results.Ratings[1].value);
+            console.log("Country Origin: " + results.Country);
+            console.log("Language: " + results.Language);
+            console.log("Plot: " + results.Plot);
+            console.log("Actors: " + results.Actors + "\n");
+        });
         break;
+    // Search for
     case "do-what-it-says":
 
         break;
     default:
-        console.log("Incorrect command. The following commands are:\n concert-this [insert song/band name]\nspotify-this-song [insert song name]\nmovie-this [insert movie name here]\ndo-what-it-says");
+        console.log("Incorrect command. The following commands are:\n\nconcert-this [insert song/band name]\nspotify-this-song [insert song name]\nmovie-this [insert movie name here]\ndo-what-it-says");
 }
